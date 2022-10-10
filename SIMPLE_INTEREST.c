abe6ad91e7cb21f91c7f2501ad1f145daa830f3c
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-10 21:59:42 +0530

    #include<stdio.h> int main() {     int p,t,r;     float result;     scanf("%d",&p);     scanf("%d",&t);     scanf("%d",&r);     result=(p*t*r)/100;     printf("%0.f",result); }

---
+++ SIMPLE_INTEREST.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int p,t,r;
+    float result;
+    scanf("%d",&p);
+    scanf("%d",&t);
+    scanf("%d",&r);
+    result=(p*t*r)/100;
+    printf("%0.f",result);
+}
