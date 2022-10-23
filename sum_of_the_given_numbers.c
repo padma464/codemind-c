245d1ba73cfbe46856aaef0950f2de78802ab16b
Yarramsetti Sai Padma <114167689+padma464@users.noreply.github.com>
2022-10-23 13:59:18 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     for(int i=0;i<t;i++)     {         int a,b,c;         scanf("%d%d",&a,&b);         c=a+b;         printf("%d ",c);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++)
+    {
+        int a,b,c;
+        scanf("%d%d",&a,&b);
+        c=a+b;
+        printf("%d
+",c);
+    }
+}
